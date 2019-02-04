419c27392ece983dfb142954eab08f4d423d0fa3
Victor Chavarro <victor.chavarro@zagalabs.com>
2019-02-04 12:06:54 -0500

    fix(master): export default ErrorBoundary component fix

--- src/error-boundary/error-boundary.js
+++ src/error-boundary/error-boundary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ErrorBoundary extends Component {
+export default class ErrorBoundary extends Component {
   state = {
     hasError: false,
     errorMessage: '',
